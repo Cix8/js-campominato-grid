22469886f380a3658f7ea2d639e7f898160060cb
Cix8 <francesco.natal_fn@libero.it>
2022-04-22 14:45:14 +0200

    add 'easy-mode_grid'

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-alert('ciao');
+const gridContainer = document.querySelector('.grid-container');
+console.log(gridContainer);
+
+const startBtn = document.getElementById('start-btn');
+console.log(startBtn);
+
+startBtn.addEventListener('click',
+    function() {
+        gridContainer.innerHTML = '';
+        for (let i = 1; i <= 100; i++) {
+            const newElement = document.createElement('div');
+            newElement.classList.add('box-easy');
+            console.log(newElement);
+        
+            gridContainer.append(newElement);
+        }
+
+        console.log(gridContainer);
+    }
+)
