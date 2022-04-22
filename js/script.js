3e65cfd6bd30208b89a6c8a7e3f3b8850bd0bbc8
Cix8 <francesco.natal_fn@libero.it>
2022-04-22 15:08:55 +0200

    add active class in css, add eventListener to every newElement('div')

--- js/script.js
+++ js/script.js
@@ -4,14 +4,29 @@ console.log(gridContainer);
 const startBtn = document.getElementById('start-btn');
 console.log(startBtn);
 
+const instructions = document.querySelector('.instructions');
+console.log(instructions);
+
 startBtn.addEventListener('click',
     function() {
+        instructions.classList.add('d-none');
         gridContainer.innerHTML = '';
+        gridContainer.classList.remove('d-none');
         for (let i = 1; i <= 100; i++) {
             const newElement = document.createElement('div');
             newElement.classList.add('box-easy');
             console.log(newElement);
-        
+
+            const newItem = `<span>${i}</span>`;
+            newElement.innerHTML = newItem;
+
+            newElement.addEventListener('click',
+                function() {
+                    this.classList.add('active')
+                    console.log(this);
+                }
+            )
+            
             gridContainer.append(newElement);
         }
 
