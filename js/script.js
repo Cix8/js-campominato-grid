5072ce50b1823bdd5c3426a5a5b0a6a98600aee7
Cix8 <francesco.natal_fn@libero.it>
2022-04-22 16:38:56 +0200

    add possibility to choose the gameMode

--- js/script.js
+++ js/script.js
@@ -7,27 +7,77 @@ console.log(startBtn);
 const instructions = document.querySelector('.instructions');
 console.log(instructions);
 
+const easyModeSize = 100;
+const hardModeSize = 81;
+const crazyModeSize = 49;
+
 startBtn.addEventListener('click',
     function() {
         instructions.classList.add('d-none');
         gridContainer.innerHTML = '';
         gridContainer.classList.remove('d-none');
-        for (let i = 1; i <= 100; i++) {
-            const newElement = document.createElement('div');
-            newElement.classList.add('box-easy');
-            console.log(newElement);
+        let gameMode = document.getElementById('difficulty-select').value;
+        console.log(gameMode);
+        if (gameMode === 'easy') {
 
-            const newItem = `<span>${i}</span>`;
-            newElement.innerHTML = newItem;
+            for (let i = 1; i <= easyModeSize; i++) {
+                const newElement = document.createElement('div');
+                newElement.classList.add('box','easy-mode');
+                console.log(newElement);
+    
+                const newItem = `<span>${i}</span>`;
+                newElement.innerHTML = newItem;
+    
+                newElement.addEventListener('click',
+                    function() {
+                        this.classList.add('active')
+                        console.log(this);
+                    }
+                )
+    
+                gridContainer.append(newElement);
+            }
 
-            newElement.addEventListener('click',
-                function() {
-                    this.classList.add('active')
-                    console.log(this);
-                }
-            )
-            
-            gridContainer.append(newElement);
+        } else if (gameMode === 'hard') {
+
+            for (let i = 1; i <= hardModeSize; i++) {
+                const newElement = document.createElement('div');
+                newElement.classList.add('box','hard-mode');
+                console.log(newElement);
+    
+                const newItem = `<span>${i}</span>`;
+                newElement.innerHTML = newItem;
+    
+                newElement.addEventListener('click',
+                    function() {
+                        this.classList.add('active')
+                        console.log(this);
+                    }
+                )
+    
+                gridContainer.append(newElement);
+            }
+
+        } else {
+
+            for (let i = 1; i <= crazyModeSize; i++) {
+                const newElement = document.createElement('div');
+                newElement.classList.add('box','crazy-mode');
+                console.log(newElement);
+    
+                const newItem = `<span>${i}</span>`;
+                newElement.innerHTML = newItem;
+    
+                newElement.addEventListener('click',
+                    function() {
+                        this.classList.add('active')
+                        console.log(this);
+                    }
+                )
+    
+                gridContainer.append(newElement);
+            }
+
         }
 
         console.log(gridContainer);
